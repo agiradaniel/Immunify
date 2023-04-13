ba7df5a33d2bcfcdecb5a9e54a8daf7d54c0d755
agiradaniel <61313029+agiradaniel@users.noreply.github.com>
2023-04-13 09:52:09 +0300

    Login Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Landing from './components/pages/landing';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUp from './components/pages/signUp';
 import SignUpTwo from './components/pages/experimental/signUpTwo';
+import LogIn from './components/pages/login';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
        <Route path="/" element={<Landing/>}/>
        <Route path="/signup" element={<SignUp/>}/>
+       <Route path="/login" element={<LogIn/>}/>
        <Route path="/signuptwo" element={<SignUpTwo/>}/>
       </Routes>
     </div>
